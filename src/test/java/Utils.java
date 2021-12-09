5806b53dbb8cfb6b829dae2af267b2404fee0971
ConnorDoherty <connor@alchemytechs.com>
2021-12-09 15:04:24 +0000

    checker

--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -1,3 +1,5 @@
 public class Utils {
   final static String BASE_URL = "https://iptvcat.com/home_11";
+  final static String GITHUB_URL = "https://github.com";
+  final static String GITHUB_PR_URL = "https://github.com/connord96/IPTV_Live_M3U_Creator/pulls";
 }
